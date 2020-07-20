3aae4fdc5c6241de028cab922450c1acfb90840f
krickarby <krickarby@gmail.com>
2020-07-20 11:18:38 -0700

    Final Chapter 3 Exercise

--- Chapter 3 Exercises.js
+++ Chapter 3 Exercises.js
@@ -17,7 +17,7 @@ function isEven(N) {
     else if (N == 1) return false
     else if (N < 0) return isEven(-N)
   }
-
+}
 
  function isEvener(N) {
   if (N == 0) return true
@@ -26,3 +26,21 @@ function isEven(N) {
   else return isEvener(N - 2)
 }
 
+// Count Character Functions
+function countBs(string) {
+  let count = 0;
+  for (let N = 0 ; N < string.length; N++)
+    if (string[N] == "B") {
+      count += 1;
+    }
+      return count
+ }
+    
+ function countChar(string, X) {
+  let count = 0;
+  for (let N = 0 ; N < string.length; N++)
+    if (string[N] == X) {
+      count += 1;
+    }
+      return count
+ } 
